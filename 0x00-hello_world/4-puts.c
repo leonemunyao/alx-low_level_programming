9a6ef3847d7c74f3b0959cfe3e293a9a21aa0f19
leonemunyao <leonemunyao@gmail.com>
2023-09-07 08:17:47 -0700

    Using puts and betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - this is the main function
+ *
+ * Return: should be 0
+ */
+
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
