613d624914bcbe05a4431730f268ba33f2be9945
leonemunyao <leonemunyao@gmail.com>
2023-09-08 09:47:00 -0700

    Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: should be 0
+ */
+
+int main(void)
+{
+int z;
+for (z = 0 ; z < 10 ; z++)
+{
+printf("%d", z);
+}
+printf("\n");
+return (0);
+}
