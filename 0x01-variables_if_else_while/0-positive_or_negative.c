e560e8b2a1bd9ed56d674d5e7a5c0cd670040111
leonemunyao <leonemunyao@gmail.com>
2023-09-08 10:48:14 -0700

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,6 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
-#include <string.h>
 
 void trim(char *s);
 /* betty style doc for function main goes there */
@@ -20,26 +19,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
+{
 printf("%d is positive", n);
+}
 else if (n == 0)
+{
 printf("%d is zero", n);
-elise
+}
+else
+{
 printf("%d is negative", n);
+}
 printf("\n");
-trim (s);
-printf("\nAfter:\n\n")
-printf("%s\n")
 return (0);
-}
-
-void trim(char *s)
-{
-int i = strlen(s) - 1;
-while (i > 0)
-{
-if (s[i] == '' || s[i] == '\n' || s[i] == '\t') 
-i--;
-else break;
-}
-s[i + 1] = '\0';
 }
