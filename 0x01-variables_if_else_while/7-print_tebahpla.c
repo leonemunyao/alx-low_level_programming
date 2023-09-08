dc806affd8b842f5b54fa6556c8eed5887f33877
leonemunyao <leonemunyao@gmail.com>
2023-09-08 10:07:08 -0700

    Lowercase letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Should be 0
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'z' ; ch >= 'a' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
