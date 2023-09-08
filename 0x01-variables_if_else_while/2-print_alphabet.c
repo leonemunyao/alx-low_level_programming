03ac91319f80fb5836122717ce23109260217be0
leonemunyao <leonemunyao@gmail.com>
2023-09-08 08:59:34 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Return: should be 0
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar (ch);
+putchar ('\n');
+}
+return (0);
+}
