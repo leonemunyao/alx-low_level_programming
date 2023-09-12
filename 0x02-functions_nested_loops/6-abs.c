996df8efe5de93aa0ced9a5a4c1d4fa33f01ca0c
leonemunyao <leonemunyao@gmail.com>
2023-09-12 15:36:42 -0700

    Returns the absolute value of the int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value
+ * @j: Parameter to be called
+ * Return: j or -j
+ */
+
+int _abs(int j)
+{
+if (j < 0)
+{
+return (-j);
+}
+else
+{
+return (j);
+}
+}
