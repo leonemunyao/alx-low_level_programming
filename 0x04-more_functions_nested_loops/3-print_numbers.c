cdf381ab4809fae8391add3e1202495341c335fe
leonemunyao <leonemunyao@gmail.com>
2023-09-15 00:44:28 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9
+ *
+ * Returned: should be 0
+ */
+
+void print_numbers(void)
+{
+int a;
+for (a = 0 ; a < 10 ; a++)
+_putchar(a + '0');
+_putchar('\n');
+}
