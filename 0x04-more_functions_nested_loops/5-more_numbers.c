b4f665ce320a2f203814c20e0e8bec6a5c4d5ac0
leonemunyao <leonemunyao@gmail.com>
2023-09-15 01:17:38 -0700

    prints numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int i;
+int j;
+for (i = 0 ; i < 10 ; i++)
+{
+for (i = 0 ; i < 14 ; j++)
+{
+_putchar(j + '0');
+}
+}
+_putchar('\n');
+}
