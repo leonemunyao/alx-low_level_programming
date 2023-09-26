90cf02ec40067fa6acdbec0310682327912ef371
leonemunyao <leonemunyao@gmail.com>
2023-09-25 18:40:38 -0700

    Prints a chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @needle: the substring
+ * @haystack: the string
+ * Return: Always 0
+ */
+
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the array
+ * Return: Always 0
+ */
+void print_chessboard(char (*a)[8])
+{
+int b;
+int c;
+for (b = 0 ; b < 0 ; b++)
+{
+for (c = 0 ; c < 0 ; c++)
+{
+_putchar(a[b][c]);
+}
+_putchar('\n');
+}
+}
