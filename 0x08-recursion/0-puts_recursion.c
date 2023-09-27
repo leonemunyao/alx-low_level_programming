71433badd5a0283fcae032fa53e04333294c9f5e
leonemunyao <leonemunyao@gmail.com>
2023-09-27 11:55:25 -0700

    Prints a string followed by a newline

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the string
+ *
+ * Return: Always 0 (success)
+ */
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
+}
