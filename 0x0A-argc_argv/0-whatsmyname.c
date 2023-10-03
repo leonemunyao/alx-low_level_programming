069f46ec02f9aa84adca98dce5b37992597f69a5
leonemunyao <leonemunyao@gmail.com>
2023-10-03 10:31:09 -0700

    Program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the programs name
+ * @argc: the number of command lines
+ * @argv: the array of argc
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
