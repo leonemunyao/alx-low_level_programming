6d835b0bc86d39a82c9d50169c0143891855146f
leonemunyao <leonemunyao@gmail.com>
2023-10-04 19:50:08 -0700

    malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments
+ * @ac: the count of argument
+ * @av: the vector of argument
+ *
+ * Return: Always 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+char *point;
+int i;
+int j;
+int k;
+
+if (ac == 0)
+{
+return (NULL);
+}
+for (i = 0 ; )
+}
