99a106bac9d2368753349afa0b1086fc3fa45af0
leonemunyao <leonemunyao@gmail.com>
2023-10-09 15:02:56 -0700

    A new type of struct called dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 char *name;
