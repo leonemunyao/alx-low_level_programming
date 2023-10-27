04d172f6a5bd84f55580d81acd9cab5367594c27
leonemunyao <leonemunyao@gmail.com>
2023-10-27 00:53:48 -0700

    Function that return the value of a bit at a given time

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,22 +1,26 @@
 #include"main.h"
 
 /**
- * get_bit - prog returns the value of a bit at a given index.
+ * get_bit - function that returns the value of a bit at a given index.
  * @n: number to check bits in
  * @index: index at which to check bit
  *
- * Return: value of the bit, or -1 if there is an error
+ * Return: Always 0
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int divisor, check;
+unsigned long int i, j;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
-		return (-1);
-	divisor = 1 << index;
-	check = n & divisor;
-	if (check == divisor)
-		return (1);
-	return (0);
+if (index > (sizeof(unsigned long int) * 8 - 1))
+{
+return (-1);
 }
-
+i = 1 << index;
+j = n & i;
+if (j == i)
+{
+return (1);
+}
+return (0);
+}
