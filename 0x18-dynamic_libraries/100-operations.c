8d0e019bf7a18e73bf23fa5e1a04678a7b27305e
leonemunyao <leonemunyao@gmail.com>
2023-12-12 04:57:26 +0300

    Creating a script that creates a dynamic library called liball.so from all .c files

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * sum - calculate the sum
+ * @a: integer
+ * @b: integer
+ * Return: Always 0
+*/
+int sum(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * sub - calculates the difference
+ * @a: integer
+ * @b: integer
+ * Return: Always 0
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * mul - calculates the product
+ * @a: integer
+ * @b: integer
+ * Return: Always 0
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * my_div - calculates the division
+ * @a: integer
+ * @b: integer
+ * Return: Always 0
+*/
+int my_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * mod - calculate the mod
+ * @a: integer
+ * @b: integer
+ * Return: Always 0
+*/
+int mod(int a, int b)
+{
+return (a % b);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -25,5 +25,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int sum(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int my_div(int a, int b);
+int mod(int a, int b);
 
 #endif
