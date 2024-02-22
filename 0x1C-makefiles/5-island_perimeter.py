586c78a663ecd7e256bf95555a78473e2b0999c3
leonemunyao <leonemunyao@gmail.com>
2024-02-22 05:53:21 +0300

    Adding pycodestyle in 5-island_perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Island Perimeter"""
 
+
 def island_perimeter(grid):
     """Return the perimeter of an island in a grid."""
     width = len(grid[0])
@@ -12,8 +13,8 @@ def island_perimeter(grid):
         for k in range(width):
             if grid[j][k] == 1:
                 size += 1
-                if ( k > 0 and grid[j][k-1] == 1 ):
+                if (k > 0 and grid[j][k-1] == 1):
                     edges += 1
-                if ( j > 0 and  grid[j-1][k] == 1 ):
+                if (j > 0 and grid[j-1][k] == 1):
                     edges += 1
     return size * 4 - edges * 2
